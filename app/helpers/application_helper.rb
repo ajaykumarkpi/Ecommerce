7156b23975093aad9a3b72070d74263613d2ff94
ajaykumarkpi <ajay.kumar@kpitechservices.com>
2023-05-08 17:24:11 +0530

    Added styling to the application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def login_helper
+    if current_user
+      content_tag :div, class: 'logout-button' do
+        button_to "Logout", destroy_user_session_path, method: :delete, class: 'logout'
+      end
+    else
+      content_tag :div, class: 'login-buttons' do
+        (link_to "Register", new_user_registration_path, class: 'register') +
+        " ".html_safe + 
+        (button_to "Login", new_user_session_path,class: 'login')  
+      end
+    end
+  end
 end
